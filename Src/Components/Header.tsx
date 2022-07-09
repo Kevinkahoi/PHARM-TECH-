5b197c2a6570de1d5303809326bb10ba0d9db3b5
Kevinkahoi <kevinkahoi@gmail.com>
2022-07-09 09:36:10 +0300

    2 files modified, 3 added

--- Src/Components/Header.tsx
+++ Src/Components/Header.tsx
@@ -1,16 +1,46 @@
 import * as React from 'react';
 
+import './Header.css';
 export default function Header() {
-  return <div>
-<ul>
-  <li>Home   </li>
-  <li> Jobs </li>
-  <li>Post Jobs </li>
-  <li> Register </li>
+  function myFunction() {
+    var x = document.getElementById('myTopnav');
+    if (x.className === 'topnav') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'topnav';
+    }
+  }
 
+  return (
+    <div>
+      <div className="topnav" id="myTopnav">
+        <a href="#home" className="active">
+          Home
+        </a>
+        <a href="#news">Jobs</a>
+        <a href="#contact">Post Jobs</a>
 
-  </ul>
+        <a href="#about">Chemist </a>
+        <a href="#about">Register</a>
+        <a href="javascript:void(0);" className="icon" onClick={myFunction}>
+          <i className="fa fa-bars"></i>
+        </a>
+      </div>
 
-
-  </div>;
+      <ul>
+        <li>
+          <a href="default.asp">Home</a>
+        </li>
+        <li>
+          <a href="news.asp"> Jobs</a>
+        </li>
+        <li>
+          <a href="contact.asp">Post Jobs</a>
+        </li>
+        <li>
+          <a href="about.asp">Register</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
